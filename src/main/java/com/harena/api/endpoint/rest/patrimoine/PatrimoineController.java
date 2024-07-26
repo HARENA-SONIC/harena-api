b0d0b61ba7665cd483fed195a283a2f5a95cc4c6
anydevlabs <andyavotiana@gmail.com>
2024-07-27 01:27:27 +0300

    add Services to feat/controller

--- src/main/java/com/harena/api/endpoint/rest/patrimoine/PatrimoineController.java
+++ src/main/java/com/harena/api/endpoint/rest/patrimoine/PatrimoineController.java
@@ -3,8 +3,10 @@ package com.harena.api.endpoint.rest.patrimoine;
 import com.harena.api.endpoint.rest.model.Patrimoine;
 import com.harena.api.service.PatrimoineService;
 import com.harena.api.service.mappers.PatrmoineMapper;
+
 import java.util.List;
 import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,30 +21,30 @@ import org.springframework.web.server.ResponseStatusException;
 @RequiredArgsConstructor
 @RequestMapping("patrimoines")
 public class PatrimoineController {
-  private final PatrimoineService patrimoineService;
-  private final PatrmoineMapper mapper;
+    private final PatrimoineService patrimoineService;
+    private final PatrmoineMapper mapper;
 
-  @PutMapping
-  public String crupdatePatrimoines(@RequestBody ListPayload<Patrimoine> entity) {
-    List<school.hei.patrimoine.modele.Patrimoine> patrimoines =
-        entity.data().stream().map(mapper::toObjectModel).toList();
-    patrimoineService.savePatrimoine(patrimoines);
-    return "OK";
-  }
-
-  @GetMapping
-  public ListPayload<Patrimoine> getPatrimoines() {
-    return new ListPayload<>(
-        patrimoineService.getAllPatrimoine(null).stream().map(mapper::toRestModel).toList());
-  }
-
-  @GetMapping("/{id}")
-  public Patrimoine getPatrimoine(@PathVariable String id) {
-    Optional<school.hei.patrimoine.modele.Patrimoine> patrimoine =
-        patrimoineService.getPatrimone(id);
-    if (patrimoine.isEmpty()) {
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    @PutMapping
+    public String crupdatePatrimoines(@RequestBody ListPayload<Patrimoine> entity) {
+        List<school.hei.patrimoine.modele.Patrimoine> patrimoines =
+                entity.data().stream().map(mapper::toObjectModel).toList();
+        patrimoineService.savePatrimoine(patrimoines);
+        return "OK";
     }
-    return mapper.toRestModel(patrimoine.get());
-  }
+
+    @GetMapping
+    public ListPayload<Patrimoine> getPatrimoines() {
+        return new ListPayload<>(
+                patrimoineService.getAllPatrimoine(null).stream().map(mapper::toRestModel).toList());
+    }
+
+    @GetMapping("/{nom_patrimoine}")
+    public Patrimoine getPatrimoine(@PathVariable(name = "nom_patrimoine") String id) {
+        Optional<school.hei.patrimoine.modele.Patrimoine> patrimoine =
+                patrimoineService.getPatrimone(id);
+        if (patrimoine.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return mapper.toRestModel(patrimoine.get());
+    }
 }
--- src/main/java/com/harena/api/service/PatrimoineService.java
+++ src/main/java/com/harena/api/service/PatrimoineService.java
@@ -1,12 +1,15 @@
 package com.harena.api.service;
 
 import com.harena.api.utils.PageRequest;
+import com.harena.api.utils.StringNormalizer;
+
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.java.Log;
@@ -18,58 +21,54 @@ import school.hei.patrimoine.serialisation.Serialiseur;
 @Service
 @RequiredArgsConstructor
 public class PatrimoineService {
-  private final String PATRIMOINE_KEY = "patrimoines.db";
-  private final BucketProvider bucketProvider;
-  private final Serialiseur<HashMap<String, Patrimoine>> serializer = new Serialiseur<>();
+    private final String PATRIMOINE_KEY = "patrimoines.db";
+    private final BucketProvider bucketProvider;
+    private final Serialiseur<HashMap<String, Patrimoine>> serializer = new Serialiseur<>();
 
-  @SneakyThrows
-  public Optional<Patrimoine> getPatrimone(String id) {
-    File patrimoineFile = bucketProvider.getBucket().download(PATRIMOINE_KEY);
-    if (!patrimoineFile.exists()) {
-      return Optional.empty();
+    @SneakyThrows
+    public Optional<Patrimoine> getPatrimone(String patrimoineName) {
+        String id = StringNormalizer.apply(patrimoineName);
+        File patrimoineFile = bucketProvider.getBucket().download(PATRIMOINE_KEY);
+        if (!patrimoineFile.exists()) {
+            return Optional.empty();
+        }
+
+        HashMap<String, Patrimoine> patrimoine =
+                serializer.deserialise(Files.readString(patrimoineFile.toPath()));
+        log.info("Getting patrimoine of Id: " + id);
+        return Optional.ofNullable(patrimoine.get(id));
     }
 
-    HashMap<String, Patrimoine> patrimoine =
-        serializer.deserialise(Files.readString(patrimoineFile.toPath()));
-    log.info("Getting patrimoine of Id: " + id);
-    return Optional.ofNullable(patrimoine.get(id));
-  }
+    @SneakyThrows
+    public List<Patrimoine> savePatrimoine(List<Patrimoine> toSavePatrimoines) {
+        File oldPatrimoinesFile = bucketProvider.getBucket().download(PATRIMOINE_KEY);
+        if (!oldPatrimoinesFile.exists()) {
+            oldPatrimoinesFile.createNewFile();
+            HashMap<String, Patrimoine> emptyPatrimoine = new HashMap<>();
+            Files.writeString(oldPatrimoinesFile.toPath(), serializer.serialise(emptyPatrimoine));
+        }
+        String oldPatrimoineString = Files.readString(oldPatrimoinesFile.toPath());
+        HashMap<String, Patrimoine> patrimoines = serializer.deserialise(oldPatrimoineString);
+        toSavePatrimoines.forEach(
+                patrimoine -> {
+                    patrimoines.put(StringNormalizer.apply(patrimoine.nom().toLowerCase()), patrimoine);
+                });
 
-  @SneakyThrows
-  public List<Patrimoine> savePatrimoine(List<Patrimoine> toSavePatrimoines) {
-    File oldPatrimoinesFile = bucketProvider.getBucket().download(PATRIMOINE_KEY);
-    if (!oldPatrimoinesFile.exists()) {
-      oldPatrimoinesFile.createNewFile();
-      HashMap<String, Patrimoine> emptyPatrimoine = new HashMap<>();
-      Files.writeString(oldPatrimoinesFile.toPath(), serializer.serialise(emptyPatrimoine));
+        String newPatrimoineString = serializer.serialise(patrimoines);
+        Path tmpFile = Files.createTempFile(PATRIMOINE_KEY, null);
+        File serializedPatrimoine = Files.writeString(tmpFile, newPatrimoineString).toFile();
+        bucketProvider.getBucket().upload(serializedPatrimoine, PATRIMOINE_KEY);
+        return toSavePatrimoines;
     }
 
-    String oldPatrimoineString = Files.readString(oldPatrimoinesFile.toPath());
-    HashMap<String, Patrimoine> patrimoines = serializer.deserialise(oldPatrimoineString);
-    toSavePatrimoines.forEach(
-        patrimoine -> {
-          patrimoines.put(normalizeName(patrimoine.nom().toLowerCase()), patrimoine);
-        });
-
-    String newPatrimoineString = serializer.serialise(patrimoines);
-    Path tmpFile = Files.createTempFile(PATRIMOINE_KEY, null);
-    File serializedPatrimoine = Files.writeString(tmpFile, newPatrimoineString).toFile();
-    bucketProvider.getBucket().upload(serializedPatrimoine, PATRIMOINE_KEY);
-    return toSavePatrimoines;
-  }
-
-  @SneakyThrows
-  public List<Patrimoine> getAllPatrimoine(PageRequest request) {
-    File patrimoinesFile = bucketProvider.getBucket().download(PATRIMOINE_KEY);
-    if (!patrimoinesFile.exists()) {
-      return List.of();
+    @SneakyThrows
+    public List<Patrimoine> getAllPatrimoine(PageRequest request) {
+        File patrimoinesFile = bucketProvider.getBucket().download(PATRIMOINE_KEY);
+        if (!patrimoinesFile.exists()) {
+            return List.of();
+        }
+        HashMap<String, Patrimoine> patrimoines =
+                serializer.deserialise(Files.readString(patrimoinesFile.toPath()));
+        return patrimoines.values().stream().toList();
     }
-    HashMap<String, Patrimoine> patrimoines =
-        serializer.deserialise(Files.readString(patrimoinesFile.toPath()));
-    return patrimoines.values().stream().toList();
-  }
-
-  private String normalizeName(String name) {
-    return name.replaceAll("\\W", "_");
-  }
 }
